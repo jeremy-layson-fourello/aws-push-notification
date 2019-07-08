5d79cba30378e6379bb61dd91d9196f0734f489b
Jeremy Layson <jeremy.layson@fourello.com>
2019-07-08 16:08:38 +0800

    composer.json

---
+++ Fourello/aws-push-notification/src/FourelloAWSPushNotificationServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Fourello\AWSPushNotification;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContactFormServiceProvider extends ServiceProvider {
+
+    public function boot()
+    {
+
+
+        // publish migrations
+        $this->publishes([
+            __DIR__ . '/../migrations/2019_07_08_173434_create_user_devices_table.php' => database_path('migrations/' . $file),
+            __DIR__ . '/../migrations/2019_07_08_173435_create_user_topics_table.php' => database_path('migrations/' . $file),
+        ]);
+
+        // publish other files
+        $this->publishes([
+            __DIR__ . '/Models' => base_path('app/Models'),
+            __DIR__ . '/Controllers' => base_path('app/Http/Controllers'),
+            __DIR__ . '/Libraries' => base_path('app/Libraries'),
+            __DIR__ . '/config/fourello-push' => config_path('fourello-push.php'),
+        ]);
+    }
+
+    public function register()
+    {
+
+    }
+}
